93833ba722c7563cf4773b9780af12a2071c39bb
dandy <danielsbarbosa27@gmail.com>
2020-12-14 23:42:23 -0300

    Add ScrollView e RaisedButton

--- calculo_imc/lib/main.dart
+++ calculo_imc/lib/main.dart
@@ -20,10 +20,63 @@ class _HomeState extends State<Home> {
         centerTitle: true,
         backgroundColor: Colors.green,
         actions: <Widget>[
-          IconButton(icon: Icon(
-              Icons.refresh),
-              onPressed: () {},)
+          IconButton(
+            icon: Icon(Icons.refresh),
+            onPressed: () {},
+          )
         ],
+      ),
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Icon(
+              Icons.person_outline,
+              size: 127.0,
+              color: Colors.green,
+            ),
+            TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                  labelText: "Peso (kg)",
+                  labelStyle: TextStyle(color: Colors.green)),
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.green, fontSize: 27.00),
+            ),
+            TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                  labelText: "Altura (cm)",
+                  labelStyle: TextStyle(color: Colors.green)),
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.green, fontSize: 27.00),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+              child: Container(
+                height: 50.0,
+                child: RaisedButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Calcular",
+                    style: TextStyle(color: Colors.white, fontSize: 27.0),
+                  ),
+                  color: Colors.green,
+                ),
+              ),
+            ),
+            Text(
+              "Info",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.green,
+                fontSize: 27.0,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
