1087c793d9855e542c1c2378971bb97c299180d7
dandy <danielsbarbosa27@gmail.com>
2020-12-15 14:19:50 -0300

    Add Formulśrios e validações

--- calculo_imc/lib/main.dart
+++ calculo_imc/lib/main.dart
@@ -15,7 +15,7 @@ class _HomeState extends State<Home> {
   TextEditingController weightControler = TextEditingController();
   TextEditingController heightControler = TextEditingController();
 
-  GlobalKey<FormState> _formKey = GlobalKey<FormState>;
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
 
   String _infoText = "Informe seus dados!";
@@ -36,17 +36,17 @@ class _HomeState extends State<Home> {
       double imc = weight / (height * height);
       print(imc);
       if (imc < 18.0) {
-        _infoText = "Abaixo do Peso (${imc.toStringAsPrecision(2)})";
+        _infoText = "Abaixo do Peso (${imc.toStringAsPrecision(3)})";
       } else if(imc >= 18.6 && imc < 24.9){
-        _infoText = "Peso Ideal (${imc.toStringAsPrecision(2)})";
+        _infoText = "Peso Ideal (${imc.toStringAsPrecision(3)})";
       } else if(imc >= 24.9 && imc < 29.9){
-        _infoText = "Levemente acima do Peso (${imc.toStringAsPrecision(2)})";
+        _infoText = "Levemente acima do Peso (${imc.toStringAsPrecision(3)})";
       } else if(imc >= 29.9 && imc < 34.9){
-        _infoText = "Obesidade Grau I (${imc.toStringAsPrecision(2)}";
+        _infoText = "Obesidade Grau I (${imc.toStringAsPrecision(3)}";
       } else if(imc >= 34.9 && imc < 39.9){
-        _infoText = "Obesidade Grau II (${imc.toStringAsPrecision(2)})";
+        _infoText = "Obesidade Grau II (${imc.toStringAsPrecision(3)})";
       }else if(imc >= 40.0){
-        _infoText = "Obesidade Grau III (${imc.toStringAsPrecision(2)})";
+        _infoText = "Obesidade Grau III (${imc.toStringAsPrecision(3)})";
       }
     });
   }
@@ -86,7 +86,11 @@ class _HomeState extends State<Home> {
                 textAlign: TextAlign.center,
                 style: TextStyle(color: Colors.green, fontSize: 27.00),
                 controller: weightControler,
-                validator: ,
+                validator: (value) {
+                  if(value.isEmpty){
+                    return "Insira eu Peso";
+                  }
+                },
               ),
               TextFormField(
                 keyboardType: TextInputType.number,
@@ -96,13 +100,22 @@ class _HomeState extends State<Home> {
                 textAlign: TextAlign.center,
                 style: TextStyle(color: Colors.green, fontSize: 27.00),
                 controller: heightControler,
+                validator: (value) {
+                  if(value.isEmpty){
+                    return "Insira eu Peso";
+                  }
+                },
               ),
               Padding(
                 padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
                 child: Container(
                   height: 50.0,
                   child: RaisedButton(
-                    onPressed: _calculate,
+                    onPressed: (){
+                      if(_formKey.currentState.validate()){
+                        _calculate();
+                      }
+                    },
                     child: Text(
                       "Calcular",
                       style: TextStyle(color: Colors.white, fontSize: 27.0),
